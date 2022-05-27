b511ad72e5dbc06e12524fb8e05a68eb372490b0
Jonas Kluger <jonas@das-element.com>
2022-05-27 11:05:55 +0200

    fix documentation logo

--- create_docs.sh
+++ create_docs.sh
@@ -1,3 +1,3 @@
 . venv/bin/activate
 mkdir -p docs
-pdoc daselement_api/api.py --logo "https://github.com/das-element/python-api/blob/main/das-element_logo_docu_api.png" -o docs
+pdoc daselement_api/api.py --logo "https://github.com/das-element/python-api/raw/main/das-element_logo_docu_api.png" -o docs
