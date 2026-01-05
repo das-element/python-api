861562a325f57dc9b565d1096c3281dc9e443392
Jonas Kluger <jonas@das-element.com>
2026-01-05 14:19:46 +0100

    update year

--- daselement_api/__init__.py
+++ daselement_api/__init__.py
@@ -4,7 +4,7 @@
 #    / /_/ / /_/ (__  )  /  __/ /  __/ / / / / /  __/ / / / /_
 #    \__,_/\__,_/____/   \___/_/\___/_/ /_/ /_/\___/_/ /_/\__/
 #
-#                  Copyright (c) 2025 das element
+#                  Copyright (c) 2026 das element
 
 from ._version import __version__
 from . import api
